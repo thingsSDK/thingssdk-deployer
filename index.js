2280c12a2fcc7dc61e9792068c8904c4483096c6
Andrew Chalkley <andrew@chalkley.org>
2016-07-24 22:35:37 -0700

    initial commit for deployer

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class Deployer {
+    constructor() {
+        this.runtimes = {};
+    }
+
+    addToRuntime(runtime, key, value) {
+        var newRuntimeObject = {};
+        newRuntimeObject[key] = value;
+        this.runtimes[runtime] = Object.assign(newRuntimeObject, this.runtimes[runtime]);
+    }
+    
+    build(runtime, builder) {
+        builder((err, code) => {
+            if(err) {
+                throw err;
+            } else {
+                this.runtimes[runtime].uploader(code);
+            }
+        });
+    }
+
+    upload(runtime, uploader) {
+        this.addToRuntime(runtime, "uploader", uploader);
+    }
+}
+
+module.exports = () => new Deployer();
